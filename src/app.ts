892d0c83943e8c6fead1f5ecb1162aa925a6f7e5
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2020-10-03 08:44:28 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import MainScene from "./scripts/scenes/MainScene";
+import "./styles/style.scss";
+import "phaser";
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  backgroundColor: "#000000",
+  height: window.innerHeight,
+  parent: "game",
+  physics: {
+    arcade: {
+      debug: false, // true for collisions debug
+    },
+    default: "arcade",
+  },
+  scene: [MainScene],
+  type: Phaser.AUTO,
+  width: document.body.offsetWidth,
+};
+// tslint:disable-next-line: no-unused-expression
+new Phaser.Game(gameConfig);
